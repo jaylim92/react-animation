cafceb742580af6db44393015069c73942db28e7
jaylim92 <jaylim3272@gmail.com>
2022-09-01 21:29:12 +0900

    Guesture

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 
@@ -9,24 +9,44 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
-const Box = styled(motion.div)`
+const BiggerBox = styled.div`
   width: 200px;
   height: 200px;
+  background-color: rgba(255, 255, 255, 0.301);
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Box = styled(motion.div)`
+  width: 50px;
+  height: 50px;
   background-color: white;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxAnimation = {
-  start: { scale: 0 },
-  end: { scale: 1, rotate: 360, transition: { type: "spring", bounce: 0.5 } },
+const boxVar = {
+  hover: { scale: 1.5, rotate: 90 },
+  click: { scale: 1, borderRadius: "400px" },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box variants={boxAnimation} initial="start" animate="end" />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          dragSnapToOrigin
+          dragElastic={0.5}
+          dragConstraints={biggerBoxRef}
+          variants={boxVar}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
